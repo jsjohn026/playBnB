91933e0c707e0a8651bcda59a20e038d82051af6
Jasmine <jsjohn026@gmail.com>
2019-09-19 20:39:59 -0500

    add booking form container

---
+++ frontend/components/bookings/booking_form_container.js
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+import BookingForm from "./booking_form";
+
+const mapStateToProps = (state, ownProps) => ({
+
+})
+
+const mapDispatchToProps = dispatch => {
+  
+}
+
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(BookingForm);
