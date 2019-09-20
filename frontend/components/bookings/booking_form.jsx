09910303eff3d194eba117e8fd107149d2d0b540
Jasmine <jsjohn026@gmail.com>
2019-09-19 20:34:02 -0500

    add booking form component

---
+++ frontend/components/bookings/booking_form.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class BookingForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        <form action="">
+          <button type="submit">Reserve</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BookingForm;
