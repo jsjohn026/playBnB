d2bd012e23298b5c2fdb46d9c1c5701cb53b70bb
Jasmine <jsjohn026@gmail.com>
2019-09-25 21:59:40 -0500

    complete booking form

--- frontend/components/bookings/booking_form.jsx
+++ frontend/components/bookings/booking_form.jsx
@@ -13,21 +13,31 @@ class BookingForm extends React.Component {
       <div className="booking-form">
         <div className="booking-form-header">
           <p><span className="listing-price">${listing.price}</span> per night</p>
-          <p className="booking-rating">{listing.average_rating || "No reviews yet"}</p>
+          <p className="booking-rating">
+            <i className="fas fa-star"></i>
+            {listing.average_rating || "No reviews yet"}
+          </p>
         </div>
 
-        <form action="">Booking Reservation
-          <label htmlFor=""> Start Date</label>
-          <input type="date" name="date" id=""/>
-          <label htmlFor="">End Date</label>
-          <input type="date" name="date" id=""/>
-          <select name="guests" id="">Number of Guests:
+        <form action="">
+          <p className="booking-form-dates">Dates
+            <div className="date-row">
+              <input type="date" name="date" id="" />
+              <input type="date" name="date" id="" />
+            </div>
+          </p>
+
+          <p className="guest-dropdown">Guests</p>
+          <select className="guest-dropdown-options" name="guests" id="">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
           </select>
           <button className="reserve-button" type="submit">Reserve</button>
+          <div className="reserve-button-note">
+            <p>You won’t be charged yet</p>
+            A small number of reservations may include an Playbnb-required security deposit.</div>
         </form>
       </div>
     )
--- frontend/components/listings/show/listing_detail.jsx
+++ frontend/components/listings/show/listing_detail.jsx
@@ -5,12 +5,19 @@ import ListingMap from "./listing_map";
 const ListingDetail = ({ listing, reviews }) => {
   return (
     <div className="listing-details">
-      <h3>{listing.name}
-        <p>{listing.city}</p>
+      <h3 className="listing-name">{listing.name}
+        <p className="listing-city">{listing.city}</p>
       </h3>
+
+      
       <ul className="listing-attributes">
-        <li>{listing.num_bedrooms} Bedrooms</li>
-        <li>{listing.max_guests} Guests</li>
+        <div className="listing-accomodations">
+          <p>{listing.max_guests} Guests</p>
+          <p>{listing.num_bedrooms} Bedrooms</p>
+          <p>{listing.num_bathrooms} Bathrooms</p>
+        </div>
+        <li><div className="attribute-heading"><i class="fas fa-home"></i> Entire home</div>
+        You'll have the entire house to yourself</li>
         <li>{listing.description}</li>
         { 
           listing.id ? <ListingMap listing={listing}/> : <div></div>
